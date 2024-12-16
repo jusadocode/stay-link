011f4cfc46669975de9bd7b03ebb965937abade2
Justas Adomaitis <justas.ado@gmail.com>
2024-12-16 03:08:10 +0200

    adding docker compose

--- stay-link.Server/Program.cs
+++ stay-link.Server/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", builder =>
