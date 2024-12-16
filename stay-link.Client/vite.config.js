b5d318bdd67c173632efc127e96a81873064b85e
Justas Adomaitis <justas.ado@gmail.com>
2024-12-16 13:26:00 +0200

    docker compose https certificate configuration

--- stay-link.Client/vite.config.js
+++ stay-link.Client/vite.config.js
@@ -31,7 +31,9 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 }
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:8080';
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:8081';
+
+console.log(target);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -52,6 +54,7 @@ export default defineConfig({
         https: {
             key: fs.readFileSync(keyFilePath),
             cert: fs.readFileSync(certFilePath),
+
         }
     }
 })
